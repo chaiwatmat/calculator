5372deae5c19dd3a8653e5908fe5b2135f2e864d
Chaiwat Matarak <chaiwat.mat.dev@gmail.com>
2017-03-05 01:20:57 +0700

    Initial

---
+++ calculator/Calculator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace chaiwatmat.calculator
+{
+    public static class Calculator
+    {
+        public static int Add(int x, int y) => x + y;
+        public static int Subtract(int x, int y) => x - y;
+        public static int Multiply(int x, int y) => x * y;
+        public static int Divide(int x, int y) => x / y;
+    }
+}
